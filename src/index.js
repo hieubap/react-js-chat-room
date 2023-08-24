871186aaecbce384641bc7a8cf246a6cc56af2a3
hieu.nq <ngohieu1811@gmail.com>
2023-08-24 22:07:05 +0700

    bỏ fontawesome

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "@fortawesome/fontawesome-free/css/all.min.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
 import PublicLayout from "@layouts/public";
 import React from "react";
 import ReactDOM from "react-dom";
